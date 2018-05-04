c51d2463e9ca9c7a40d00ac0c74a9ae558e704e8
Isaac Gariano <isaac.oscar.gariano@anu.edu.au>
2018-05-04 18:16:44 +1000

    Git submodule

--- jikesrvm_test.fish
+++ jikesrvm_test.fish
@@ -4,8 +4,7 @@ if not test -e ./benchmarks/dacapo-2006-10-MR2.jar
 	wget https://downloads.sourceforge.net/project/dacapobench/archive/2006-10-MR2/dacapo-2006-10-MR2.jar -O benchmarks/dacapo-2006-10-MR2.jar
 end
 
-if not test -d ./jikesrvm
-	git clone git@gitlab.anu.edu.au:mmtk/jikesrvm.git
+if test -d ./jikesrvm/rust_mmtk
 	rmdir ./jikesrvm/rust_mmtk
 	ln -s ../           ./jikesrvm/rust_mmtk
 	ln -s ../benchmarks ./jikesrvm/benchmarks
